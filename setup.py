9256762903ff18229aa71503ab0221e25d768975
satyam_capri <satyam.gupta2@capriglobal.in>
2023-02-23 17:35:02 +0530

    ic

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name='loggerPackage',
+    version='1.2',
+    author='Satyam_Gupta',
+    author_email='satyamguptaece@gmail.com',
+    description='add logger while logging',
+    long_description="it will add logs",
+    long_description_content_type="text/markdown",
+    url='https://github.com/satyamgupta0202/loggerPackage',
+    project_urls = {
+        "Bug Tracker": "https://github.com/satyamgupta0202/loggerPackage/issues"
+    },
+    license='MIT',
+    packages=['loggerPackage']
+)
